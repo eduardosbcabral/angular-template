39654cfe9f2620591337662ff04cfeda1f3743f0
Eduardo da Silva Barros Cabral <eduardo.cabral@cg.df.gov.br>
2019-07-29 16:16:44 -0300

    Pequenas correções

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -3,25 +3,31 @@ import { DrawerComponent } from '../drawer/drawer.component';
 import { AuthenticationService } from 'src/app/core/auth/authentication.service';
 import { Router } from '@angular/router';
 import { Usuario } from 'src/app/pages/usuarios/usuario.model';
-
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   @ViewChild(DrawerComponent, {static: false})
   drawer: DrawerComponent;
 
   currentUser: Usuario;
 
+  applicationName: string;
+
   constructor(
     private router: Router,
     private authenticationService: AuthenticationService
   ) {
     this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
+
+  ngOnInit() {
+    this.applicationName = environment.applicationName;
   }
 
   logout() {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  applicationName: 'Angular App',
   production: false,
   apiUrl: 'http://localhost:4200',
 };
